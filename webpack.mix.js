c30103de333855e60e0fc1fdf71540f759eb5c5e
José González <maangx@gmail.com>
2020-08-16 23:28:56 -0400

    Go back to conventions with the build folder

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,15 +1,15 @@
 const mix = require('laravel-mix');
 const tailwindcss = require('tailwindcss');
 
-mix.copy('src/index.html', 'docs/index.html')
-    .copyDirectory('src/data', 'docs/data')
-    .react('src/js/app.jsx', 'docs/js/app.js')
-    .sass('src/css/app.scss', 'docs/css/app.css')
+mix.copy('src/index.html', 'build/index.html')
+    .copyDirectory('src/data', 'build/data')
+    .react('src/js/app.jsx', 'build/js/app.js')
+    .sass('src/css/app.scss', 'build/css/app.css')
 
     .combine([
         'node_modules/@fortawesome/fontawesome-free/css/all.css',
-    ], 'docs/css/vendor.css')
-    .copyDirectory('node_modules/@fortawesome/fontawesome-free/webfonts', 'docs/webfonts')
+    ], 'build/css/vendor.css')
+    .copyDirectory('node_modules/@fortawesome/fontawesome-free/webfonts', 'build/webfonts')
 
     .options({
         processCssUrls: false,
